cd82898e1b086ab1e086e8b09e7cbe5be0f30d2f
pasha <lisovchenko.pasha@gmail.com>
2017-11-09 16:07:08 +0200

    ADD permission_classes

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import detail_route
 from rest_framework.viewsets import ModelViewSet
 from product.models import Product, Category
 from .serializer import ProductSerializer, CategorySerializer
@@ -5,10 +6,12 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class ProductViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
 
 class CategoryViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
